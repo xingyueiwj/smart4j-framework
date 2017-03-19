4b479cf949b041600c7b9aa5b8c8eb2b57b1c755
yangwanbiao <ywb10.8@qq.com>
2017-03-20 02:11:21 +0800

    mvc框架

---
+++ smart-framework/src/main/java/org/smart4j/framework/helper/DatabaseHelper.java
@@ -0,0 +1,317 @@
+package org.smart4j.framework.helper;
+
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.MapListHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.smart4j.framework.util.CollectionUtil;
+import org.smart4j.framework.util.PropsUtil;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * 数据库操作助手类
+ * Created by Administrator on 2017/2/15 0015.
+ */
+public final class DatabaseHelper {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseHelper.class);
+//    private static final String DRIVER;
+//    private static final String URL;
+//    private static final String USERNAME;
+//    private static final String PASSWORD;
+    /**
+     * 使用dbutil的QueryRunner方法
+     */
+    private static final QueryRunner QUERY_RUNNER;
+
+    /**
+     * 使用线程容器装载保存sql连接
+     */
+    private static final ThreadLocal<Connection> CONNECTION_HOLDER;
+
+    /**
+     * 连接池对象
+     */
+    private static final BasicDataSource DATA_SOURCE;
+
+    static {
+        CONNECTION_HOLDER = new ThreadLocal<Connection>();
+        QUERY_RUNNER = new QueryRunner();
+        Properties conf = PropsUtil.loadProps("config.properties");
+//        DRIVER = conf.getProperty("jdbc.driver");
+//        URL = conf.getProperty("jdbc.url");
+//        USERNAME = conf.getProperty("jdbc.username");
+//        PASSWORD = conf.getProperty("jdbc.password");
+
+         String driver = conf.getProperty("jdbc.driver");
+         String url = conf.getProperty("jdbc.url");
+         String username = conf.getProperty("jdbc.username");
+         String password = conf.getProperty("jdbc.password");
+
+        DATA_SOURCE = new BasicDataSource();
+        DATA_SOURCE.setDriverClassName(driver);
+        DATA_SOURCE.setUrl(url);
+        DATA_SOURCE.setUsername(username);
+        DATA_SOURCE.setPassword(password);
+//        try{
+//            Class.forName(DRIVER);
+//        } catch (ClassNotFoundException e){
+//            LOGGER.error("can not load jdbc driver", e);
+//        }
+    }
+    /**
+     * 开启事务
+     */
+    public static void beginTransaction(){
+        Connection conn = getConnection();
+        if (conn != null){
+            try {
+                conn.setAutoCommit(false);
+            }catch (SQLException e){
+                LOGGER.error("begin transaction failure",e);
+                throw new RuntimeException(e);
+            }finally {
+                CONNECTION_HOLDER.set(conn);
+            }
+        }
+    }
+
+    /**
+     * 提交事务
+     */
+    public static void commitTransaction(){
+        Connection conn = getConnection();
+        if (conn != null){
+            try {
+                conn.commit();
+                conn.close();
+            }catch (SQLException e){
+                LOGGER.error("commit transaction failure",e);
+                throw new RuntimeException(e);
+            }finally {
+                CONNECTION_HOLDER.remove();
+            }
+        }
+    }
+
+    /**
+     * 查询实体列表
+     * @param entityClass
+     * @param sql
+     * @param params
+     * @param <T>
+     * @return
+     */
+    public static <T> List<T> queryEntityList(Class<T>entityClass,String sql,Object... params){
+        List<T> entityList;
+        try {
+            Connection conn = getConnection();
+            entityList = QUERY_RUNNER.query(conn,sql,new BeanListHandler<T>(entityClass),params);
+        }catch (SQLException e){
+            LOGGER.error("query entity list failure",e);
+            throw new RuntimeException(e);
+        }finally {
+            closeConnection();
+        }
+        return entityList;
+    }
+
+    /**
+     * 查询单个实体
+     * @param entityClass
+     * @param sql
+     * @param params
+     * @param <T>
+     * @return
+     */
+    public static <T> T queryEntity(Class<T>entityClass,String sql,Object... params){
+        T entity;
+        try {
+            Connection conn = getConnection();
+            entity = QUERY_RUNNER.query(conn,sql,new BeanHandler<T>(entityClass),params);
+        }catch (SQLException e){
+            LOGGER.error("query entity list failure",e);
+            throw new RuntimeException(e);
+        }finally {
+            closeConnection();
+        }
+        return entity;
+    }
+
+    /**
+     * 执行查询语句
+     * @param sql
+     * @param param
+     * @return
+     */
+    public static List<Map<String,Object>> executeQuery(String sql,Object... param){
+        List<Map<String,Object>> result;
+        try {
+            Connection conn = getConnection();
+            result = QUERY_RUNNER.query(conn,sql,new MapListHandler(),param);
+        }catch (Exception e){
+            LOGGER.error("excute query failure",e);
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+
+    /**
+     * 执行更新语句
+     * @param sql
+     * @param params
+     * @return
+     */
+    public static int excuteUpdate(String sql,Object... params){
+        int rows =0;
+        try {
+            Connection conn = getConnection();
+            rows = QUERY_RUNNER.update(conn, sql, params);
+        }catch (SQLException e){
+            LOGGER.error("excute update failure",e);
+            throw new RuntimeException(e);
+        }finally {
+            closeConnection();
+        }
+        return rows;
+    }
+
+    /**
+     * 插入实体
+     * @param entityClass
+     * @param fieldMap
+     * @param <T>
+     * @return
+     */
+    public static <T> boolean insertEntity(Class<T>entityClass,Map<String,Object>fieldMap){
+        if (CollectionUtil.isEmpty(fieldMap)){
+            LOGGER.error("can not insert entity:fieldMap is empty");
+            return false;
+        }
+        String sql = "INSERT INTO " + getTableName(entityClass);
+        StringBuilder columns = new StringBuilder("(");
+        StringBuilder values = new StringBuilder("(");
+        for (String fieldName:fieldMap.keySet()){
+            columns.append(fieldName).append(", ");
+            values.append("?, ");
+        }
+        columns.replace(columns.lastIndexOf(", "),columns.length(),")");
+        values.replace(values.lastIndexOf(", "),values.length(),")");
+        sql += columns + " VALUES " + values;
+        Object[] params = fieldMap.values().toArray();
+        return excuteUpdate(sql,params) == 1;
+    }
+
+    /**
+     * 更新实体
+     * @param entityClass
+     * @param id
+     * @param fieldMap
+     * @param <T>
+     * @return
+     */
+    public static <T> boolean updateEntity(Class<T> entityClass,long id,Map<String,Object>fieldMap){
+        if(CollectionUtil.isEmpty(fieldMap)){
+            LOGGER.error("can not update entity: fieldMap is empty");
+            return false;
+        }
+        String sql = "UPDATE " + getTableName(entityClass) + " SET ";
+        StringBuilder columns = new StringBuilder();
+        for (String fieldName:fieldMap.keySet()){
+            columns.append(fieldName).append("=?, ");
+        }
+        sql += columns.substring(0,columns.lastIndexOf(", ")) + " WHERE id=?";
+        List<Object> paramList = new ArrayList<Object>();
+        paramList.addAll(fieldMap.values());
+        paramList.add(id);
+        Object [] params = paramList.toArray();
+        return excuteUpdate(sql,params)==1;
+    }
+
+    /**
+     * 删除实体
+     * @param entityClass
+     * @param id
+     * @param <T>
+     * @return
+     */
+    public static <T> boolean deleteEntity(Class<T> entityClass,long id){
+        String sql = "DELETE FROM " + getTableName(entityClass) +" WHERE id=?";
+        return excuteUpdate(sql,id)==1;
+    }
+
+    /**
+     * 获取实例名称
+     * @param entityClass
+     * @return
+     */
+    private static String getTableName(Class<?>entityClass){
+        return entityClass.getSimpleName();
+    }
+
+    /**
+     * 执行sql文件
+     * @param filePath
+     */
+    public static void excuteSqlFile(String filePath){
+        InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream(filePath);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        try {
+            String sql;
+            while ((sql=reader.readLine())!=null){
+                excuteUpdate(sql);
+            }
+        }catch (Exception e){
+            LOGGER.error("excute sql file failure",e);
+        }
+    }
+    /**
+     * 获取数据库连接
+     * @return
+     */
+    public static Connection getConnection(){
+        Connection conn = CONNECTION_HOLDER.get();
+        if (conn==null){
+            try {
+//                conn = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+                conn = DATA_SOURCE.getConnection();
+            }catch (SQLException e){
+                LOGGER.error("get connection failure",e);
+                //将异常给jvm,中断程序.
+                throw new RuntimeException(e);
+            }finally {
+                CONNECTION_HOLDER.set(conn);
+            }
+        }
+        return conn;
+    }
+
+    /**
+     * 关闭数据库连接
+     */
+    public static void closeConnection(){
+        Connection conn = CONNECTION_HOLDER.get();
+        if(conn!=null){
+            try {
+                conn.close();
+            }catch (SQLException e){
+                LOGGER.error("close connection failure",e);
+                throw new RuntimeException(e);
+            }finally {
+                CONNECTION_HOLDER.remove();
+            }
+        }
+    }
+}
